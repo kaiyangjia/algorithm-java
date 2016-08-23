f6d08c2f1c6b240eb39d2f68a9914e15f230b18b
kaiyangjia <kaiyangjia@163.com>
2016-08-23 14:23:37 +0800

    init

---
+++ base/src/main/java/com/jiakaiyang/java/algorithm/App.java
@@ -0,0 +1,11 @@
+package com.jiakaiyang.java.algorithm;
+
+/**
+ * the main class
+ */
+public class App {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ base/src/main/java/com/jiakaiyang/java/algorithm/sorting/SimpleSorting.java
@@ -0,0 +1,8 @@
+package com.jiakaiyang.java.algorithm.sorting;
+
+/**
+ * 简单的排序实现,针对的是数字的简单的常用的排序
+ */
+public class SimpleSorting {
+
+}
